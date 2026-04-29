c0f4d7db09a929564c318811d455d534a7b7bbfe
Lauren Hennelly <lmhennelly@ucdavis.edu>
2026-04-28 23:05:17 -0500

    Update 01_scripts.sh

--- 02_phylogeny/01_scripts.sh
+++ 02_phylogeny/01_scripts.sh
@@ -1,1 +1,12 @@
 
+
+mamba create -n buscogeny -c bioconda -c conda-forge busco
+
+conda activate buscogeny
+
+mamba install -c bioconda -c conda-forge \
+    biopython pandas numpy mafft iqtree clipkit \
+    matplotlib plotly maskrc-svg clonalframeml
+pip install alive-progress
+
+
