8e43b4c2385c0165423935d8449af15f1cbb31f1
Lauren Hennelly <lmhennelly@ucdavis.edu>
2026-04-29 11:57:49 -0500

    Update 01_scripts.sh

--- 02_phylogeny/01_scripts.sh
+++ 02_phylogeny/01_scripts.sh
@@ -13,3 +13,22 @@ pip install alive-progress
 cd /storage/hpc/work/lh106/BlackBackedJackals/Buscogeny
 python buscogeny.py --help
 
+# Works 
+
+#!/bin/bash
+#SBATCH --job-name=guamrail
+#SBATCH --account=commons
+#SBATCH --partition=commons
+#SBATCH --ntasks=1
+#SBATCH --mem=30G
+#SBATCH --time=14:00:00
+#SBATCH --output=/home/lh106/Rice/Dog_Selection/slurmout/guamrail.out
+#SBATCH --error=/home/lh106/Rice/Dog_Selection/slurmout/guamrail.err
+
+
+# Activate environment
+conda activate buscogeny
+
+# Run Buscogeny
+python /scratch/lh106/software/Buscogeny/buscogeny.py  -i /scratch/lh106/GuamRail/genomeassemblies/  -d /scratch/lh106/GuamRail/odb10_dataset_2/aves_odb10   -o /scratch/lh106/GuamRail/output 
+
