9544e53421cc541e000407ca467a8f0b3b5e75e5
Lauren Hennelly <lmhennelly@ucdavis.edu>
2026-05-01 12:48:30 -0500

    Update 01_scripts.sh

--- 02_phylogeny/01_scripts.sh
+++ 02_phylogeny/01_scripts.sh
@@ -26,8 +26,8 @@ python buscogeny.py --help
 #SBATCH --ntasks=1
 #SBATCH --mem=50G
 #SBATCH --time=2-23:00:00
-#SBATCH --output=/home/lh106/Rice/Dog_Selection/slurmout/guamrail.out
-#SBATCH --error=/home/lh106/Rice/Dog_Selection/slurmout/guamrail.err
+#SBATCH --output=/home/lh106/Rice/Dog_Selection/slurmout/guamrailtree.out
+#SBATCH --error=/home/lh106/Rice/Dog_Selection/slurmout/guamrailtree.err
 
 
 # Activate environment
@@ -36,3 +36,4 @@ conda activate buscogeny
 # Run Buscogeny
 python /scratch/lh106/software/Buscogeny/buscogeny.py  -i /scratch/lh106/GuamRail/genomeassemblies/  -d /scratch/lh106/GuamRail/odb10_dataset_2/aves_odb10   -o /scratch/lh106/GuamRail/output 
 
+/scratch/lh106/bin/iqtree-3.1.1-Linux-intel/bin/iqtree3 -s /scratch/lh106/GuamRail/output_Apr29_Buscogeny_out/supermatrix/superaln.degapped.fasta -B 1000 --prefix /scratch/lh106/GuamRail/output_Apr29_Buscogeny_out/iqtree/iqtree
